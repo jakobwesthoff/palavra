926dc7c763a0d906d56f8c5a0a5925115cbc3d35
Jakob Westhoff <jakob@westhoffswelt.de>
2016-08-06 13:07:24 +0200

    Added support for sass styles

--- Gulpfile.babel.js
+++ Gulpfile.babel.js
@@ -5,67 +5,90 @@ import rimraf from 'rimraf';
 import run from 'run-sequence';
 import chokidar from 'chokidar';
 import mkdirp from 'mkdirp-promise';
+import beepbeep from 'beepbeep';
 import {webpackConfig} from './webpack.config.js';
 
 const plugins = loadPlugins();
 
 function chokidarWatch(glob, fn) {
-    const watcher = chokidar.watch(glob);
-    watcher.on('ready', () => {
-        watcher.on('all', (...args) => fn(...args));
-    });
+  const watcher = chokidar.watch(glob);
+  watcher.on('ready', () => {
+    watcher.on('all', (...args) => fn(...args));
+  });
 }
 
 function chokidarWatchRun(glob, ...targets) {
-    chokidarWatch(glob, (event, filepath) => run(...targets));
+  chokidarWatch(glob, (event, filepath) => run(...targets));
 }
 
 gulp.task('clean', next => {
-    rimraf('./Distribution', next);
+  rimraf('./Distribution', next);
 });
 
 gulp.task('prepare:directories', () => Promise.all([
-    mkdirp('Distribution'),
-    mkdirp('Distribution/Library'),
-    mkdirp('Distribution/Pages'),
+  mkdirp('Distribution'),
+  mkdirp('Distribution/Library'),
+  mkdirp('Distribution/Pages'),
 ]));
 
 gulp.task('prepare', ['prepare:directories']);
 
 gulp.task('copy:pages', () => {
-    return gulp.src('Pages/**/*')
-        .pipe(gulp.dest('./Distribution/Pages/'));
+  return gulp.src('Pages/**/*')
+    .pipe(gulp.dest('./Distribution/Pages/'));
 });
 
 gulp.task('copy:manifest', () => {
-    return gulp.src('manifest.json')
-        .pipe(gulp.dest('./Distribution/'));
+  return gulp.src('manifest.json')
+    .pipe(gulp.dest('./Distribution/'));
 });
 
 gulp.task('copy', ['copy:pages', 'copy:manifest']);
 
 gulp.task('webpack', next => {
-    webpack(webpackConfig, (err, stats) => {
-        if (err) {
-            throw new plugins.util.PluginError('webpack', err);
-        }
+  webpack(webpackConfig, (err, stats) => {
+    if (err) {
+      throw new plugins.util.PluginError('webpack', err);
+    }
 
-        plugins.util.log('[webpack]', stats.toString());
-        next();
-    });
+    plugins.util.log('[webpack]', stats.toString());
+    next();
+  });
+});
+
+gulp.task('sass', () => {
+  return gulp.src(['Styles/**/*.{scss,sass}', '!Styles/**/_*.{scss,sass}'])
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.plumber(error => {
+      beepbeep();
+      console.error(chalk.red('Error compiling Sass file.')); // eslint-disable-line no-console
+      console.error(chalk.red(error.message)); // eslint-disable-line no-console
+      this.emit('end');
+    }))
+    .pipe(plugins.sass({
+      precision: 8,
+      errLogToConsole: false,
+    }))
+    .pipe(plugins.plumber.stop())
+    .pipe(plugins.autoprefixer({
+      browsers: 'last 5 Chrome versions',
+    }))
+    .pipe(plugins.sourcemaps.write('./', {sourceRoot: null}))
+    .pipe(gulp.dest('Distribution/Styles/'));
 });
 
 gulp.task('build', next => run(
-    'clean',
-    'prepare',
-    ['copy', 'webpack'],
-    next
+  'clean',
+  'prepare',
+  ['copy', 'sass', 'webpack'],
+  next
 ));
 
 gulp.task('watch', ['default'], () => {
-    chokidarWatchRun('Pages/**/*', 'copy:pages');
-    chokidarWatchRun('manifest.json', 'copy:manifest');
-    chokidarWatchRun('Application/**/*', 'webpack');
+  chokidarWatchRun('Pages/**/*', 'copy:pages');
+  chokidarWatchRun('manifest.json', 'copy:manifest');
+  chokidarWatchRun('Application/**/*', 'webpack');
+  chokidarWatchRun('Styles/**/*', 'sass');
 });
 
 gulp.task('default', ['build']);
