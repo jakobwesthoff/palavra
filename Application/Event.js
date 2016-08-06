9cc1d08ba16cfe0c35080e39c57dda10fee78b86
Jakob Westhoff <jakob@westhoffswelt.de>
2016-08-06 22:12:09 +0200

    Load data from storage background page init

--- Application/Event.js
+++ Application/Event.js
@@ -1,3 +1,5 @@
+/* global chrome */
+
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {wrapStore, alias} from 'react-chrome-redux';
@@ -5,14 +7,22 @@ import {wrapStore, alias} from 'react-chrome-redux';
 import rootReducer from './Reducers';
 import aliases from './Aliases';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    alias(aliases),
-    thunk
-  )
-);
+chrome.storage.local.get('markdown', storage => {
+  let markdown = '';
+  if (chrome.runtime.lastError === undefined) {
+    markdown = storage.markdown;
+  }
+  const store = createStore(
+    rootReducer,
+    {markdown},
+    applyMiddleware(
+      alias(aliases),
+      thunk
+    )
+  );
 
-wrapStore(store, {
-  portName: 'Palavra',
+  wrapStore(store, {
+    portName: 'Palavra',
+  });
 });
+
