eb74aecd5ee656a8d6939b960ff9f1b9a65569eb
Jakob Westhoff <jakob@westhoffswelt.de>
2016-08-06 22:45:46 +0200

    Ensure background page is present when newtab is loaded

--- Application/NewTab.js
+++ Application/NewTab.js
@@ -1,4 +1,4 @@
-/* global document */
+/* global document, chrome */
 
 import React from 'react';
 import {render} from 'react-dom';
@@ -7,17 +7,21 @@ import {Store} from 'react-chrome-redux';
 
 import Palavra from 'Components/Palavra';
 
-const store = new Store({
-  portName: 'Palavra',
+// Make sure background is ready before running any init code
+chrome.runtime.getBackgroundPage(() => {
+  const store = new Store({
+    portName: 'Palavra',
+  });
+
+  // Render after first store sync
+  const unsubscribe = store.subscribe(() => {
+    unsubscribe(); // one shot only
+    render(
+      <Provider store={store}>
+        <Palavra />
+      </Provider>,
+      document.getElementById('palavra')
+    );
+  });
 });
 
-// Render after first store sync
-const unsubscribe = store.subscribe(() => {
-  unsubscribe(); // one shot only
-  render(
-    <Provider store={store}>
-      <Palavra />
-    </Provider>,
-    document.getElementById('palavra')
-  );
-});
